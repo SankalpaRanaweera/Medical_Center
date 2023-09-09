db9657943d2dd72d6795dc4dc42bcdb14c375827
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-09-09 11:17:04 -0700

    added pages,components

---
+++ medical_center/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Accordion from 'react-bootstrap/Accordion';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './pages/Home';
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
