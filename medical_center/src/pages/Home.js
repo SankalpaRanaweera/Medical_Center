6733cf92f6e4d6c4bd3d8cb74bf3f28190bf35ed
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-09-15 20:47:53 -0700

    add some things

--- medical_center/src/pages/Home.js
+++ medical_center/src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nbar from '../components/Nbar'
+const photo = new URL("./src/pages/istockphoto-1189304032-612x612.jpg",import.meta.url)
 export default function Home() {
   return (
     <div>
       <Nbar/>
       <div className="homediv">
         <h1>hi</h1>
+        <img src="[photo]"/>
 
       </div>
 
