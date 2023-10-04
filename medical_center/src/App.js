cb62271e2fd47863662f5f94333b9bc9af4328ef
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-10-04 06:06:42 -0700

    add someparts

--- medical_center/src/App.js
+++ medical_center/src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
 import Home from './pages/Home';
 import Appoinment from './pages/Appoinment';
-import{BrowserRouter as Router,Route,Switch, Routes}from'react-router-dom'
+
 function App() {
   return (
-    <Router>
-    <div className="App">
-     <switch>
-      <Routes exact path="/">
-      <Home/>
-      </Routes>
-      <Routes path="/Appoinment">
-        <Appoinment/>
-        </Routes>
-        </switch>
-    </div>
-    </Router>
+    <>
+    <Home/>
+    <Appoinment/>
+    </>
   );
-}
+  }
 
 
 export default App;
--- medical_center/src/pages/Home.js
+++ medical_center/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Nbar from '../components/Nbar'
-import img1 from '../images/images.jpg'
+import img1 from '../images/istockphoto-1189304032-612x612.jpg'
 export default function Home() {
   return (
     <div>
