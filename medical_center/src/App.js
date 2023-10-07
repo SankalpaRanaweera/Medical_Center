a0bd93f576d92bf483f8851d889f365a892ca006
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-10-06 20:48:12 -0700

    update some part

--- medical_center/src/App.js
+++ medical_center/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Nbar from './components/Nbar';
 import Home from './pages/Home';
 import Appoinment from './pages/Appoinment';
+import Doctors from './pages/Doctors';
 
 function App() {
   return (
     <>
     <Nbar/>
-    <Home/>
-    <Appoinment/>
+   {/* <Home/>
+    <Appoinment/>*/}
+    <Doctors/>
    
     </>
   );
---
+++ medical_center/src/pages/Doctors.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function Doctors() {
+  return (
+    <div className='doctorsdiv'>
+      <div className='ddiv1'>
+
+
+      </div>
+      <div className='ddiv2'>
+
+      </div>
+      <div className='ddiv3'>
+
+      </div>
+      <div className='ddiv4'>
+
+      </div>
+      <div className='ddiv5'>
+
+      </div>
+    </div>
+  )
+}
