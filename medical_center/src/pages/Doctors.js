13dbf81ad9a8e63325b2ee0a464902333332f49b
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2023-10-07 12:07:52 -0700

    add some parts

--- medical_center/src/pages/Doctors.js
+++ medical_center/src/pages/Doctors.js
@@ -1,22 +1,69 @@
 import React from 'react'
+{/*import img1 from '../images/istockphoto-1189304032-612x612.jpg'
+import img1 from '../images/istockphoto-1189304032-612x612.jpg'
+import img1 from '../images/istockphoto-1189304032-612x612.jpg'
+import img1 from '../images/istockphoto-1189304032-612x612.jpg'*/}
 
 export default function Doctors() {
   return (
     <div className='doctorsdiv'>
       <div className='ddiv1'>
-
-
+         {/*<img src={img1} alt ="sfsf" className="homeimg"/>*/}
+       <span>Surgeon</span><br/>
+        <strong>Name : </strong><br/>
+        <span> DR Supun Perera</span><br/>
+        <strong>Qualifications :</strong><br/>
+        <span>MS,MBBS,FRCS(Eng) FRCS(Ed),</span><br/>
+        <span>FICS,FCS Sri Lanka </span><br/>
+        <span>Surgeon/Gastroenterology </span><br/>
+        <span>Laparoscopic Surgeon-Endoscopy </span><br/>
+        <strong>Specialisations :</strong><br/>
+        <span>Laparoscopic Surgery </span><br/>
+        <span>General Surgery </span><br/> 
+        <span>Thyroid Surgery</span> <br/>
+        <span> Breast Surgery</span>
       </div>
       <div className='ddiv2'>
+      {/*<img src={img1} alt ="sfsf" className="homeimg"/>*/}
+      <span>Ent Surgeon</span><br/>
+        <strong>Name :</strong><br/>
+        <span>DR Kamal Jayarathna</span><br/>
+        <strong>Qualifications :</strong><br/>
+        <span>MBBS,MS,DOHNS,FEB,ORL-HNS </span><br/>
+        <span>FRCSEd  ORL-HNS</span><br/>
+        <span>Consultant ENT and Head and </span><span>Neck Surgeon</span><br/>
+        <strong>Specialisations :</strong><br/>
+        <span>Endoscopic Ear Surgery</span><br/>
+        <span>Septoplasty</span><br/>
+        <span>Thyroidectomy</span><br/>
+        <span>Tonsillectomy Adenoidectomy</span>
 
       </div>
       <div className='ddiv3'>
+      {/*<img src={img1} alt ="sfsf" className="homeimg"/>*/}
+       <span>Physician Special Clinic</span><br/>
+        <strong>Name :</strong><br/>
+        <span>DR Chandimal Kumara</span><br/>
+        <strong>Qualifications :</strong><br/>
+        <span>MBBS(Colombo),MD,MPhill(Lond) </span><br/>
+        <span>F.R.C.P.(Lond),FCCP,D.Path</span><br/>
+        <strong>Specialisations :</strong><br/>
+        <span>Special interest in Blood diseases</span>
 
       </div>
       <div className='ddiv4'>
+      {/*<img src={img1} alt ="sfsf" className="homeimg"/>*/}
+       <span>Rheuatologist</span><br/>
+        <strong>Name :</strong><br/>
+        <span>DR Harindu Wijesinghe</span><br/>
+        <strong>Qualifications :</strong><br/>
+        <span> MBBS(S.L),MD(Col),MRCP(UK)</span><br/>
+        <span>Consultant Rheumatologist</span><br/>
+        <span>Specialist in Sport Medicine </span><br/>
+        <strong>Specialisations :</strong><br/>
+        <span>Rheumatology</span><br/>
+        <span>Sports Medicine</span>
 
-      </div>
-      <div className='ddiv5'>
 
       </div>
     </div>
