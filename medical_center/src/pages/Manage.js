b3090838ce0d59767fc59eaf75921b958a7bc191
Sankalpa Ranaweera <sankalparanaweera9@gmail.com>
2025-04-27 08:13:46 -0700

    add some parts

---
+++ medical_center/src/pages/Manage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Manage() {
+  return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                </tr>
+            </thead>
+        </table>
+      
+    </div>
+  )
+}
+
+export default Manage
